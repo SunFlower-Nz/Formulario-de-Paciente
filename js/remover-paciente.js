12d3dfcb7a0501e3381c2b6ed4864d7ef38d0069
Eduardo Tavares <59029161+SunFlower-Nz@users.noreply.github.com>
2020-09-17 16:16:04 -0300

    Remoção do Paciente

---
+++ js/remover-paciente.js
@@ -0,0 +1,7 @@
+var paciente = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.remove();
+});
